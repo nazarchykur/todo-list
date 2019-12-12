ab3a315f8325a77bd06e65b1c46dca206c3c3b17
nazarchykur <nazarchykur@gmail.com>
2019-12-12 17:53:53 +0200

    10. Model and Model Attributes

--- src/main/java/com/mvcpro/controller/DemoController.java
+++ src/main/java/com/mvcpro/controller/DemoController.java
@@ -1,9 +1,13 @@
 package com.mvcpro.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
 
@@ -14,11 +18,21 @@ public class DemoController {
     }
 
     // http://localhost:8080/todo-list/welcome
-    // prefix + name + suffix
-    // /WEB-INF/view/welcome.jsp
     @ResponseBody
     @GetMapping("welcome")
-    public String welcome() {
+    public String welcome(Model model) {
+        model.addAttribute("user", "Nazar");
+        log.info("model = {}", model);
+
+        // prefix + name + suffix
+        // /WEB-INF/view/welcome.jsp
         return "welcome";
     }
+
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage(){
+        log.info("welcomeMessage() called");
+        return "Welcome to this Demo App";
+    }
+
 }
