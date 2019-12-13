ce1c4fe9562894133d57961eccad1ee4f3d881d2
nazarchykur <nazarchykur@gmail.com>
2019-12-13 13:30:20 +0200

    12. Request Parameters
    welcome(@RequestParam String user, @RequestParam int age,  Model model) {
            model.addAttribute("helloMessage", demoService.getHelloMessage(user));
            model.addAttribute("age", age);

--- src/main/java/com/mvcpro/controller/DemoController.java
+++ src/main/java/com/mvcpro/controller/DemoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
@@ -31,8 +32,9 @@ public class DemoController {
 
     // http://localhost:8080/todo-list/welcome
     @GetMapping("welcome")
-    public String welcome(Model model) {
-        model.addAttribute("helloMessage", demoService.getHelloMessage("Nazar"));
+    public String welcome(@RequestParam String user, @RequestParam int age,  Model model) {
+        model.addAttribute("helloMessage", demoService.getHelloMessage(user));
+        model.addAttribute("age", age);
         log.info("model = {}", model);
 
         // prefix + name + suffix
